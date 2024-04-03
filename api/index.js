4ef0170cd8e41a1e75f0ef038419d91b78cef3e3
MUFFANUJ <anujsinghhero292@gmail.com>
2024-04-04 04:12:33 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,15 @@
-import { log } from "console";
+// import { log } from "console";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to database")
+}).catch((err)=>{
+    console.log(err);
+})
 const app = express();
 
 app.listen(6969 , ()=>{
